fd6d08ae34a6493d1039ad1ac057b0999a4fbfb1
londydz <londydz@gmail.com>
2022-06-03 05:31:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-
 /**
  * main -Entry point
  *
- *print size of different characters
+ * print size of different characters
  *
  * Return: Always Success return 0
  *
  */
-
-int maiv(void)
+int main(void)
 {
-printf("Size of a char: %ld byte(s)\n",sizeof(char));
-printf("Size of a int: %ld byte(s)\n",sizeof(int)); 
-printf("Size of a long: %ld byte(s)\n",sizeof(long int)); 
-printf("Size of a long long: %ld byte(s)\n",sizeof(long long int)); 
-printf("Size of a float: %ld byte(s)\n",sizeof(float)); 
-return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int)); 
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int)); 
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int)); 
+	printf("Size of a float: %ld byte(s)\n", sizeof(float)); 
+	return (0);
 }
