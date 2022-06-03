0bad324c63ebb9675315eb4c2bc6232c900a6a3d
londydz <londydz@gmail.com>
2022-06-03 01:02:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@
 
 int maiv(void)
 {
-printf("Size of a char: %Id byte(s)\n",sizeof(char));
-printf("Size of a int: %Id byte(s)\n",sizeof(int)); 
-printf("Size of a long: %Id byte(s)\n",sizeof(long int)); 
-printf("Size of a long long: %Id byte(s)\n",sizeof(long long int)); 
-printf("Size of a float: %Id byte(s)\n",sizeof(float)); 
+printf("Size of a char: %ld byte(s)\n",sizeof(char));
+printf("Size of a int: %ld byte(s)\n",sizeof(int)); 
+printf("Size of a long: %ld byte(s)\n",sizeof(long int)); 
+printf("Size of a long long: %ld byte(s)\n",sizeof(long long int)); 
+printf("Size of a float: %ld byte(s)\n",sizeof(float)); 
 return (0);
 }
