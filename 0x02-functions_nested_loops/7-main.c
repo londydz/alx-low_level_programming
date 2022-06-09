2c99a11e4617452869f0470d8d17e013698b3768
londydz <londydz@gmail.com>
2022-06-08 22:13:47 -0700

    7 main

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -10,10 +10,8 @@ int main(void)
 {
 	int r;
 
-	print_last_digit(98);
 	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
+	r = print_last_digit(INT_MIN);
+	_putchar('0' + r)
 	return (0);
 }
